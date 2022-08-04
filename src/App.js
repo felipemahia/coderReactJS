90591bd0f0b8b979bb98f87ac2c0a759235d39c6
Felipe Mahia <felipemahia95@gmail.com>
2022-08-04 02:39:35 -0300

    Puliendo primera entrega final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap';
 import './components/NavBar/NavBar.scss';
 import CardContainer from './components/CardContainer/CardContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 function Header() {
   /* fetch('../utils/productsMock.js')
@@ -18,14 +18,19 @@ function Header() {
       <div>
         <NavBar />
       </div>
+{/*       <Routes>
+        <Route path='/Sufi' element={<CardContainer />}/>
+        <Route path='/' element={<CardContainer />}/>
+        <Route path='/' element={<CardContainer />}/>
+      </Routes> */}
       <div className='container'>
         <div className='row'>
-        <CardContainer section="Hamburguesas" /> {/* como hago acá para pasar solo los type:'hamburguesa' del mock? */}
-        <CardContainer section="Sushi" /> {/* idem pero sushi */}
-        <CardContainer section="Otros éxitos ♥" /> {/* idem pero otros exitos xD */}
+          <CardContainer section="Hamburguesas" /> {/* como hago acá para pasar solo los type:'hamburguesa' del mock? */}
+          <CardContainer section="Sushi" /> {/* idem pero sushi */}
+          <CardContainer section="Otros éxitos ♥" /> {/* idem pero otros exitos xD */}
         </div>
         <div className='container'>
-        <ItemDetailContainer />
+          <ItemDetailContainer />
         </div>
       </div>
     </div>
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,4 +1,48 @@
 import Card from 'react-bootstrap/Card';
+import item from '../../utils/itemDetailMock';
+
+const ItemDetail2 = ({ type }) => {
+    const [itemDetail, setItemDetail] = useState([]);
+
+    const getProducts = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(item)
+        }, 2000)
+    })
+
+    useEffect(() => {
+        getProducts
+            .then((res) => {
+                setItemDetail(res);
+            })
+            .catch((error) => {
+            })
+            .finally(() => {
+            })
+    }, [])
+
+    return (
+        <>
+        <h2>{type}</h2>
+        <Card className="bg-dark text-white">
+            <Card.Img src={img1} alt="Card image" />
+            <Card.ImgOverlay>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Card.Text>
+                    {stock}
+                </Card.Text>
+                <Card.Text>{price}</Card.Text>
+            </Card.ImgOverlay>
+        </Card>
+        </>
+    )
+}
+
+
+
 
 const ItemDetail = ({ data }) => {
     const { title, description, price, img1, img2, stock } = data
