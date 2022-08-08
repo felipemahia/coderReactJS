3623b3c5d151382d9595853c710c2fa664d6c482
Felipe Mahia <felipemahia95@gmail.com>
2022-08-08 03:20:44 -0300

    Primera entrega final - corrigiendo visual de cards

--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -8,26 +8,28 @@ import Row from 'react-bootstrap/Row';
 
 
 const ItemProduct = ({ data }) => {
-    const { id, title, price, description, img, stock } = data
+    const { id, title, price, description, img, /* stock */ } = data
     return (
-        <Link to={`/products/${id}`}>
-            <Row xs={1} md={4}>
-                <Col>
-                    <Card border="secondary" style={{ width: '16rem' }}>
-                        <Card.Img variant="top" src={img} />
-                        <Card.Body>
-                            <Card.Title>{title}</Card.Title>
-                            <Card.Text>
-                                {description}, {price}
-                            </Card.Text>
-                            <Card.Text className='derecha'>{stock}</Card.Text>
-                            <ItemCount />
+
+        <Row xs={1} md={4}>
+            <Col>
+                <Card border="secondary" style={{ width: '16rem' }}>
+                    <Card.Img variant="top" src={img} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                            {description}, {price}
+                        </Card.Text>
+                        {/* <Card.Text className='derecha'>{stock}</Card.Text> */}
+                        <ItemCount />
+                        <Link to={`/products/${id}`}>
                             <Button variant="primary">Comprar</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Link>
+                        </Link>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+
     );
 }
 
