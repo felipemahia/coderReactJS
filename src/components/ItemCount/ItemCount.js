ba6831982ffbee134c5d56609c47c1d5531d70c0
Felipe Mahia <felipemahia95@gmail.com>
2022-08-09 23:46:34 -0300

    Desafio Sincronizar counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -15,12 +15,11 @@ const ItemCount = ({ stock, setQuantitySelected, id }) => {
         setQuantitySelected(counter)
     }
 
-
     return (
         <div className='contadorProductos'>
             <Button variant="outline-danger" size="sm" onClick={oneLess} disabled={counter === 1}>-</Button>
             <Link to={`/products/${id}`}>
-                {' '}<Button onClick={onAdd} variant="outline-primary">Agregar {`${counter}`} al carrito</Button>{' '}
+                <Button onClick={onAdd} variant="outline-primary">Agregar {`${counter}`} al carrito</Button>
             </Link>
             <Button variant="outline-danger" size="sm" onClick={oneMore} disabled={setCounter === stock}>+</Button>
         </div>
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,13 @@
+import React, {useState}  from 'react';
 import ItemCount from '../ItemCount/ItemCount'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import { Link } from 'react-router-dom';
 
 const ItemDetail = ({ data }) => {
-    const {title, price, description, img, stock} = data;
-    return(
+    const { title, price, description, img, stock } = data;
+    const [quantitySelected, setQuantitySelected] = useState(0)
+    return (
         <Card border="secondary" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={img} />
             <Card.Body>
@@ -16,7 +18,10 @@ const ItemDetail = ({ data }) => {
                 <Card.Text>
                     <p>Tenemos un stock de: {stock}</p>
                 </Card.Text>
-                <ItemCount/>
+                {
+                    quantitySelected > 1 ? <Button variant="outline-primary"><Link to="/cart">Confirmar compra</Link></Button> :
+                        <ItemCount stock={stock} setQuantitySelected={setQuantitySelected} />
+                }
             </Card.Body>
         </Card>
     )
