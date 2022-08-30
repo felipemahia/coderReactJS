4687b892052fc2f518e4e31f5bd57f4a08ba0919
Felipe Mahia <felipemahia95@gmail.com>
2022-08-30 18:01:17 -0300

    Buscando soluciones para firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -5,25 +5,44 @@ import ItemDetail from '../ItemDetail/ItemDetail';
 import { useParams } from "react-router-dom";
 import "./ItemDetailContainer.scss"
 import Container from "react-bootstrap/esm/Container";
+//import database from '../../firebase/config'
 
 const ItemDetailContainer = () => {
 
     const [producto, setProducto] = useState({})
     const { detalleId } = useParams()
 
+    const getItem = async () => {
+        const database = getFirestore();
+        const docRef = doc(database, "products", detalleId)
+        const docSnap = await getDoc(docRef)
+        const product = { ...docSnap.data(), id: docSnap.id }
+        return product
+    }
+    useEffect(() => {
+        const ItemAwait = async () => {
+            try {
+                const responseLog = await getItem()
+                setProducto(responseLog)
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+        ItemAwait()
+    }, [detalleId])
     //const filterId = products.find (products => products.id === Number(id))
-
     /* const getItem = () => new Promise ((resolve, reject )=>{
         setTimeout(() => {
             resolve(filterId)
         }, 2000);
     }) */
-    useEffect(() => {
+    /*useEffect(() => {
         const querydb = getFirestore();
         const queryDoc = doc(querydb, 'productos', detalleId);
         getDoc(queryDoc)
-            .then(res => setProducto({id: res.id, ...res.data()}));
-        /* const ItemAwait = async () => {
+            .then(res => setProducto({ id: res.id, ...res.data() }));
+         const ItemAwait = async () => {
             try{
                 const responseLog = await getItem ()
                 setProducto(responseLog)
@@ -32,8 +51,8 @@ const ItemDetailContainer = () => {
                 console.log(error);
             }
         }
-        ItemAwait() */
-    }, [detalleId])
+        ItemAwait()
+    }, [detalleId])*/
     return (
 
         <div className="container">
